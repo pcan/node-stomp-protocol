6a31a046b87d41323c451243be7ca7ed2ef57066
Piero Cangianiello <piero.cangianiello@magicsolution.net>
2018-04-13 11:51:26 +0200

    Simplified session factory methods signatures

--- src/index.ts
+++ src/index.ts
@@ -11,14 +11,17 @@ export { StompServerSessionLayer, StompClientSessionLayer };
 export * from './protocol'
 export * from './model'
 
-export function createStompServerSession(socket: Socket | WebSocket, listener: StompCommandListenerConstructor<StompClientCommandListener, StompSessionLayer<StompClientCommandListener>> | StompClientCommandListener, config?: StompConfig): StompServerSessionLayer {
+export type StompClientCommandListenerConstructor = StompCommandListenerConstructor<StompClientCommandListener, StompSessionLayer<StompClientCommandListener>>;
+export type StompServerCommandListenerConstructor = StompCommandListenerConstructor<StompServerCommandListener, StompSessionLayer<StompServerCommandListener>>;
+
+export function createStompServerSession(socket: Socket | WebSocket, listener: StompClientCommandListenerConstructor | StompClientCommandListener, config?: StompConfig): StompServerSessionLayer {
     const streamLayer = openStream(socket);
     const frameLayer = new StompFrameLayer(streamLayer);
     frameLayer.headerFilter = config && config.headersFilter || frameLayer.headerFilter;
     return new StompServerSessionLayer(frameLayer, listener);
 }
 
-export function createStompClientSession(socket: Socket | WebSocket, listener: StompCommandListenerConstructor<StompServerCommandListener, StompSessionLayer<StompServerCommandListener>> | StompServerCommandListener, config?: StompConfig): StompClientSessionLayer {
+export function createStompClientSession(socket: Socket | WebSocket, listener: StompServerCommandListenerConstructor | StompServerCommandListener, config?: StompConfig): StompClientSessionLayer {
     const streamLayer = openStream(socket);
     const frameLayer = new StompFrameLayer(streamLayer);
     frameLayer.headerFilter = config && config.headersFilter || frameLayer.headerFilter;
