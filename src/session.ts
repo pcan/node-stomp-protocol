64a55a333cb8a824a63b9aa7fc6960dff583adc0
Piero Cangianiello <piero.cangianiello@magicsolution.net>
2018-10-05 15:35:04 +0200

    Removing recepit management from session layer

--- src/session.ts
+++ src/session.ts
@@ -102,11 +102,9 @@ export class StompServerSessionLayer extends StompSessionLayer<StompClientComman
 
     constructor(frameLayer: StompFrameLayer, listener: StompCommandListenerConstructor<StompServerSessionLayer, StompClientCommandListener> | StompClientCommandListener) {
         super(frameLayer, listener);
-        const a = this as StompSessionLayer<StompClientCommandListener>;
     }
 
     protected handleFrame(command: StompCommand<StompClientCommandListener>, frame: StompFrame) {
-        const receipt = frame.command !== 'CONNECT' && frame.headers && frame.headers.receipt || undefined;
         const acceptVersion = frame.command === 'CONNECT' && frame.headers && frame.headers['accept-version'];
         if (this.data.authenticated || frame.command === 'CONNECT') {
             try {
@@ -117,9 +115,6 @@ export class StompServerSessionLayer extends StompSessionLayer<StompClientComman
                 super.handleFrame(command, frame);
             } catch (error) {
                 const headers: StompHeaders = { message: error.message };
-                if (receipt) {
-                    headers['receipt-id'] = receipt;
-                }
                 this.error(headers, error.details).catch(this.internalErrorHandler);
             }
         } else {
